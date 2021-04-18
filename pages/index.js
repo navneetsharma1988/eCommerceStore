2fab2162ea3a4bc17a77299cb4a23b7a66f02d0f
Navneet Sharma <navneet.sharma@admiral.at>
2021-04-18 12:48:05 +0530

    final app

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,47 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css'
 
-import products from "../products.json";
+import { useCart } from '../hooks/use-cart.js';
+
+import products from '../shared/products.json';
 
 export default function Home() {
+
+  const { addToCart } = useCart();
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Space Jelly Shop</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Space Jelly Shop</h1>
-
         <p className={styles.description}>
-          The best space jelly fish swag in the universe!
+          The best space jellyfish swag on the web!
         </p>
 
         <ul className={styles.grid}>
-          {products.map((product) => {
-            const { id, title, price, description, image } = product;
+          {products.map(product => {
+            const { id, title, image, description, price } = product;
             return (
               <li key={id} className={styles.card}>
-                <a href="#">
-                  <img src={image} alt={title}></img>
-                  <h3>{title}</h3>
-                  <p>${price}</p>
-                  <p>{description}</p>
-                </a>
+                <Link href={`/products/${id}`}>
+                  <a>
+                    <img src={image} alt={title} />
+                    <h3>{ title }</h3>
+                    <p>${ price }</p>
+                    <p>{ description }</p>
+                  </a>
+                </Link>
+                <p>
+                  <button className={styles.button} onClick={() => addToCart({ id })}>
+                    Buy
+                  </button>
+                </p>
               </li>
-            );
+            )
           })}
         </ul>
       </main>
@@ -41,10 +52,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  );
+  )
 }
